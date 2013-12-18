3af2bea40110eb5afc62c297f06a3cdf3176188d
Julien Guy <jguy@lbl.gov>
2013-12-17 17:39:37 -0800

    working on boost io for psf, remove analytic psf class

---
+++ src/tests/specex_test_psf_xml_io.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+#include "harp.hpp"
+
+#include "specex_message.h"
+#include "specex_psf.h"
+#include "specex_psf_io.h"
+#include "specex_gauss_hermite_analytic_psf.h"
+#include "specex_serialization.h"
+
+using namespace std;
+
+int main() {
+  
+  specex_set_verbose(true);
+  
+  
+  if(0){
+    specex::PSF_p psf(new specex::GaussHermitePSF());
+    std::ofstream os("toto.xml");
+    boost::archive::xml_oarchive xml_oa ( os );
+    xml_oa << BOOST_SERIALIZATION_NVP(psf);
+    os.close();
+  }
+  
+  if(0){
+    std::ifstream is("toto.xml");
+    boost::archive::xml_iarchive xml_ia ( is );
+    specex::PSF_p psf;
+    xml_ia >> BOOST_SERIALIZATION_NVP(psf);
+    is.close();    
+  }
+  if(0){
+    specex::PSF_p psf(new specex::GaussHermitePSF());
+    specex::write_psf_xml(psf,"toto.xml");
+  }
+  if(0){
+    specex::PSF_p psf;
+    specex::read_psf_xml(psf,"toto.xml");
+  }
+  if(1) {
+    specex::PSF_p psf;
+    specex::read_psf_xml(psf,"psf.xml");
+    specex::write_psf_fits(psf,"psf2.fits",500,2000,4);
+    specex::write_psf_xml(psf,"psf2.xml");
+  }
+  return EXIT_SUCCESS;
+}
