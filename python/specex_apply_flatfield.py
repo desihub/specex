ebb0dd3fe9f61678bfa42ffc8bac096824d21549
Julien Guy <jguy@lbl.gov>
2014-03-19 17:57:38 -0700

    propagate noise

--- python/specex_apply_flatfield.py
+++ python/specex_apply_flatfield.py
@@ -17,8 +17,15 @@ spectra=hdulist[0].data
 invar=hdulist[1].data
 hdulist2=pyfits.open(inflatfilename)
 flat=hdulist2[0].data
+invarflat=hdulist2[1].data
 spectra /= flat
-invar *= flat*flat
+# scale invar and add noise of flat field itself (ignore uncertainty on the mean flat continuum)
+# var_1 = var_0/flat/flat + ((sigma(flat)/flat**2)*spectra_0)**2
+#       = var_0/flat/flat + ((sigma(flat)/flat)*spectra_1)**2
+#       = var_0/flat/flat + var_flat/(flat*flat)*(spectra_1*spectra_1)
+#       = 1/(invvar_0*flat*flat) + 1/(invarflat*flat*flat)*(spectra_1*spectra_1)
+ 
+invvar = 1/(1/(invar*flat*flat)+ 1/(invarflat*flat*flat)*spectra*spectra)
 
 if os.path.isfile(outspecfilename) :
     os.unlink(outspecfilename)
