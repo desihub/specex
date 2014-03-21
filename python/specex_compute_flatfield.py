0b33cdfa4431679800cf808b5591945cf4c1276c
Julien Guy <jguy@lbl.gov>
2014-03-21 13:46:39 -0700

    apply just a smoothing kernel [0.25,0.5,0.25] to the flat that suffer from artificial ringing

--- python/specex_compute_flatfield.py
+++ python/specex_compute_flatfield.py
@@ -22,35 +22,27 @@ median_flat=numpy.median(spectra, axis=0)
 spectra /= median_flat
 invar *= median_flat*median_flat
 
-# we do a spline fit here because we know the relative transmission
-# from one fiber to another is a smooth function of wavelength
-step1=10 # A, this is the chosen spline resolution
-knots1=wave[0]+step1/2+step1*numpy.arange(int((wave[-1]-wave[0])/step1))
+# we don't want to do any smoothing at all because there are sharp jumps due to dead columns
+# + feature of pixel size in the middle of the CCD
+# but currently , with specter extractions, anticorrelations between adjacent pixels not
+# reflected in the noise
+# we apply a smoothing kernel [0.25,0.5,0.25]
 
-
-# we "expect" the transmission to be much smoother than observed, must be a issue with
-# spectro-perf or psf
-# compute spline on a much smoother grid to evaluate rms and assign error
-step2=200 # A, this is the chosen spline resolution
-knots2=wave[0]+step2/2+step2*numpy.arange(int((wave[-1]-wave[0])/step2))
-
-
+kernel=numpy.array([0.25,0.5,0.25])
 for fiber in range(spectra.shape[0]) :
-    tck1=scipy.interpolate.splrep(wave,spectra[fiber],w=invar[fiber],task=-1,t=knots1,k=1) # linear
-    spectra[fiber]=scipy.interpolate.splev(wave,tck1,der=0)
-    tck2=scipy.interpolate.splrep(wave,spectra[fiber],w=invar[fiber],task=-1,t=knots2,k=3) # 3rd order
-    smooth=scipy.interpolate.splev(wave,tck2,der=0)
-    chi2pdf=numpy.sum(invar[fiber]*(spectra[fiber]-smooth)**2)/(wave.shape[0]-knots2.shape[0]+2)
-    invar[fiber]/=chi2pdf
-    print "fiber",fiber,"mean flat error=",1./sqrt(numpy.mean(invar[fiber]))
-    # impossible to compute easily the variance !!!
+    toto=numpy.convolve(spectra[fiber],kernel,mode="same")
+    # fix bundaries
+    toto[0]=numpy.dot(spectra[fiber][:2],kernel[1:])/numpy.sum(kernel[1:])
+    toto[-1]=numpy.dot(spectra[fiber][-2:],kernel[:2])/numpy.sum(kernel[:2])
+    spectra[fiber]=toto
+
 
 hdulist.writeto(outfilename,clobber=True)
 
 if not meanflatfilename == "" :
-    median_flat_array=numpy.zeros(spectra.shape)
+    median_flat_array=numpy.zeros((1,wave.shape[0]))
     median_flat_array[0]=median_flat
-    median_flat_invar=numpy.ones(spectra.shape)
+    median_flat_invar=numpy.zeros(median_flat_array.shape)
     hdu0 = pyfits.PrimaryHDU(median_flat_array)
     hdu1 = pyfits.ImageHDU(median_flat_invar)
     hdu2 = pyfits.ImageHDU(wave)
