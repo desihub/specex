5edc2a03a0583b7e60b6ee75972e0fbb7b2761b8
Julien Guy <jguy@lbl.gov>
2014-03-22 14:07:51 -0700

    fix issue of ref/value

--- python/specex_convert_wavelength_air_to_vacuum.py
+++ python/specex_convert_wavelength_air_to_vacuum.py
@@ -14,7 +14,7 @@ outfilename   = sys.argv[2]
 
 hdulist       = pyfits.open(infilename)
 wave          = hdulist[2].data # in air
-wave          = convert_air_to_vacuum(wave) # now in vacuum
+hdulist[2].data = convert_air_to_vacuum(wave) # now in vacuum
 
 print "writing result to",outfilename
 hdulist.writeto(outfilename,clobber=True)
