d4e1507791c8f3c7317c2164dd1b16b607e67801
Julien Guy <jguy@lbl.gov>
2014-03-22 10:39:01 -0700

    adapted to another version of scipy, it's a pain ...

--- python/specex_cholesky.py
+++ python/specex_cholesky.py
@@ -2,17 +2,14 @@
 import numpy,scipy,scipy.linalg
 
 def cholesky_solve_and_invert(A,B,overwrite=False,lower=False) :
-    a=scipy.linalg.asarray(A)
-    b=scipy.linalg.asarray(B)
-    posv, = scipy.linalg.get_lapack_funcs(('posv',), (a,))
-    L,X,status=posv(a,b,lower=lower,overwrite_a=overwrite)
+    posv, = scipy.linalg.get_lapack_funcs(('posv',), (A,))
+    L,X,status=posv(A,B,lower=lower,overwrite_a=overwrite)
     
     if status :
         raise Error("cholesky_solve_and_invert error dposv status=",status)
     
-    l=scipy.linalg.asarray(L)
-    potri, = scipy.linalg.get_lapack_funcs(('potri',), (l,))
-    inv,status=potri(l,lower=(not lower)) # this is not a mistake, there is a BUG in scipy!!!!   
+    potri, = scipy.linalg.get_lapack_funcs(('potri',), (L,))
+    inv,status=potri(L,lower=(not lower)) # 'not lower' is not a mistake, there is a BUG in scipy!!!!   
         
     if status :
         raise Error("cholesky_solve_and_invert error dpotri status=",status)
