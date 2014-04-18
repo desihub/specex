2f36fc496516969f346ad963e3fa27a9b8c06fbc
Julien Guy <jguy@lbl.gov>
2014-04-17 17:06:29 -0700

    add cholesky_solve routine

--- python/specex_cholesky.py
+++ python/specex_cholesky.py
@@ -1,18 +1,30 @@
 
 import numpy,scipy,scipy.linalg
 
+def cholesky_solve(A,B,overwrite=False,lower=False) :
+    posv, = scipy.linalg.get_lapack_funcs(('posv',), (A,))
+    L,X,status=posv(A,B,lower=lower,overwrite_a=overwrite)
+    
+    if status :
+        print "error cholesky_solve_and_invert error dposv status=",status
+        raise Error("cholesky_solve_and_invert error dposv status=%d"%status)
+    
+    return X
+
 def cholesky_solve_and_invert(A,B,overwrite=False,lower=False) :
     posv, = scipy.linalg.get_lapack_funcs(('posv',), (A,))
     L,X,status=posv(A,B,lower=lower,overwrite_a=overwrite)
     
     if status :
-        raise Error("cholesky_solve_and_invert error dposv status=",status)
+        print "error cholesky_solve_and_invert error dposv status=",status
+        raise Error("cholesky_solve_and_invert error dposv status=%d"%status)
     
     potri, = scipy.linalg.get_lapack_funcs(('potri',), (L,))
     inv,status=potri(L,lower=(not lower)) # 'not lower' is not a mistake, there is a BUG in scipy!!!!   
         
     if status :
-        raise Error("cholesky_solve_and_invert error dpotri status=",status)
+        print "error cholesky_solve_and_invert error dpotri status=",status
+        raise Error("cholesky_solve_and_invert error dpotri status=%d"%status)
 
     #symmetrize Ai
     if True :
