f8bf149c32bce2d6936c23334b0d666db27dd0ec
Julien Guy <jguy@lbl.gov>
2016-03-14 12:13:46 +0100

    faster plotting

--- python/specex_plot_psf_params.py
+++ python/specex_plot_psf_params.py
@@ -5,20 +5,9 @@ import astropy.io.fits as pyfits
 
 from numpy.polynomial.legendre import legval
 
-def eval(pname,fiber,wavelength,verbose=False) :
-    legdeg=hdulist[1].header["LEGDEG"]
-    table=hdulist[1].data
-    paramindex=numpy.where(table["PARAM"]==pname)[0][0]
-    wavemin=table["WAVEMIN"][paramindex]
-    wavemax=table["WAVEMAX"][paramindex]
-    fiber_coeffs=table["COEFF"][paramindex][fiber]
-    
-    
+def eval(pname,fiber,wavelength) :
     wr=2*(wavelength-(wavemin+wavemax)/2)/(wavemax-wavemin)
-    val=legval(wr,fiber_coeffs)
-    if verbose :
-        print pname,"fiber=",fiber,"wave=",wavelength,"wrange=",wavemin,wavemax,"coefs=",fiber_coeffs,"val=",val
-    return val
+    return legval(wr,fiber_coeffs[fiber])
 
 if len(sys.argv)<3 :
     print sys.argv[0],"psf.fits param"
@@ -35,16 +24,19 @@ wavemax=hdulist[1].data["WAVEMAX"][paramindex]
 wave=numpy.arange(wavemin,wavemax,10)
 fibermin=int(hdulist[1].header["FIBERMIN"])
 fibermax=int(hdulist[1].header["FIBERMAX"])
+ 
+legdeg=hdulist[1].header["LEGDEG"]
+table=hdulist[1].data
+fiber_coeffs=table["COEFF"][paramindex]
 
-#fibers=numpy.arange(0,20,1)
-fibers=numpy.arange(fibermin,fibermax+1,10)
-for fiber in fibers :
-    print fiber
-    x=eval(param,fiber-fibermin,wave)
-    pylab.plot(wave,x)
+if 0 :
+    fibers=numpy.arange(fibermin,fibermax+1,10)
+    for fiber in fibers :
+        x=eval(param,fiber-fibermin,wave)
+        pylab.plot(wave,x)
 
-pylab.xlabel("wavelength")
-pylab.ylabel(param)
+    pylab.xlabel("wavelength")
+    pylab.ylabel(param)
 
 pylab.figure()
 wave=(wavemin+wavemax)/2.
@@ -52,8 +44,7 @@ x=numpy.zeros((fibermax+1-fibermin))
 fibers=numpy.arange(fibermin,fibermax+1)
 for fiber in fibers :
     x[fiber-fibermin]=eval(param,fiber-fibermin,wave)
-pylab.plot(fibers,x)
-pylab.plot(fibers,x,"o")
+pylab.plot(fibers,x,"o-")
 pylab.xlabel("fiber #")
 pylab.ylabel(param)
 
