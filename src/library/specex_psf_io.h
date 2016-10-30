bc1fc57548cc75eaeaa44e8cf02df0d3b35fdc9d
Julien Guy <jguy@lbl.gov>
2016-10-30 11:20:24 +0100

    write additional XTRACE and YTRACE hdus, because independent from PSF model (but keep it in PSF binary table for backward compatibility), add extname=PSF for the PSF binary table

--- src/library/specex_psf_io.h
+++ src/library/specex_psf_io.h
@@ -13,6 +13,8 @@ namespace specex {
   void write_psf_fits_image(const specex::PSF_p psf, const string& filename, 
 			    const int fiber, const double& wavelength, int oversampling=1);
 
+  void write_xtrace_fits_hdu(const specex::PSF& psf, fitsfile *fp, int hdu);
+  void write_ytrace_fits_hdu(const specex::PSF& psf, fitsfile *fp, int hdu);
   
   void write_psf_fits(const specex::PSF_p psf, fitsfile* fp, int first_hdu=1);
   void write_psf_fits(const specex::PSF_p psf, const string& filename);
