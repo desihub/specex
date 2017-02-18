79a9c640abe41c2fb8b3c7af37fae7f563065982
Julien Guy <jguy@lbl.gov>
2017-02-18 17:43:20 +0100

    output filename option

--- src/tests/specex_test_psf_io.cc
+++ src/tests/specex_test_psf_io.cc
@@ -16,11 +16,13 @@ using namespace std;
 int main(int argc, char *argv[]) {
   
   
-  string filename="";
+  string input_filename="";
+  string output_filename="";
   popts::options_description desc ( "Allowed Options" );
   desc.add_options()
     ( "help,h", "display usage information" )
-    ( "in,i", popts::value<string>( &filename ), "input psf filename" );
+    ( "in,i", popts::value<string>( &input_filename ), "input psf filename (xml or fits)" )
+  ( "out,o", popts::value<string>( &output_filename ), "output psf filename (xml or fits)" );
 
   popts::variables_map vm;
   try {
@@ -30,15 +32,20 @@ int main(int argc, char *argv[]) {
     cerr << "error in arguments" << endl;
     return EXIT_FAILURE;
   }
-  if(filename=="") {
+  if(input_filename=="") {
     cerr << "need input filename" << endl;
     return EXIT_FAILURE;
   }
+  if(output_filename=="") {
+    cerr << "need input filename" << endl;
+    return EXIT_FAILURE;
+  }
+  
   specex_set_verbose(true);
   specex::PSF_p psf;
-  specex::read_psf(psf,filename);
-  specex::write_psf_xml(psf,"toto.xml");
-  specex::write_psf_fits(psf,"toto.fits");
+  specex::read_psf(psf,input_filename);
+  specex::write_psf(psf,output_filename);
+  
   
   return EXIT_SUCCESS;
 }
