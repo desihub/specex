35a77557a6293be6bb369aa6dc647e2544ba5ff5
Julien Guy <jguy@lbl.gov>
2017-09-14 14:32:58 -0700

    can test bundle selection in specex_test_psf_io

--- src/tests/specex_test_psf_io.cc
+++ src/tests/specex_test_psf_io.cc
@@ -18,32 +18,48 @@ int main(int argc, char *argv[]) {
   
   string input_filename="";
   string output_filename="";
+  int first_bundle=-1;
+  int last_bundle=-1;
+  
   popts::options_description desc ( "Allowed Options" );
   desc.add_options()
     ( "help,h", "display usage information" )
     ( "in,i", popts::value<string>( &input_filename ), "input psf filename (xml or fits)" )
-  ( "out,o", popts::value<string>( &output_filename ), "output psf filename (xml or fits)" );
-
+    ( "out,o", popts::value<string>( &output_filename ), "output psf filename (xml or fits)" )
+    ( "first-bundle", popts::value<int>( &first_bundle ), "first bundle" )
+    ( "last-bundle", popts::value<int>( &last_bundle ), "first bundle" )    
+    ( "debug", "turn on debug mode" );
+  
   popts::variables_map vm;
   try {
     popts::store(popts::command_line_parser( argc, argv ).options(desc).run(), vm);
     popts::notify(vm);
   }catch(std::exception e) {
     cerr << "error in arguments" << endl;
+    cerr << "try --help for options" << endl;
     return EXIT_FAILURE;
+  }
+  if ( ( argc < 2 ) || vm.count( "help" ) ) {
+      cerr << endl;
+      cerr << desc << endl;      
+      return EXIT_FAILURE;
   }
   if(input_filename=="") {
     cerr << "need input filename" << endl;
+    cerr << "try --help for options" << endl;
     return EXIT_FAILURE;
   }
   if(output_filename=="") {
     cerr << "need output filename" << endl;
+    cerr << "try --help for options" << endl;
     return EXIT_FAILURE;
   }
   
   specex_set_verbose(true);
+  specex_set_debug(vm.count("debug")>0);
+  
   specex::PSF_p psf;
-  specex::read_psf(psf,input_filename);
+  specex::read_psf(psf,input_filename,first_bundle,last_bundle);
   specex::write_psf(psf,output_filename);
   
   
