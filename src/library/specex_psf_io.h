23019a4ebf7e5318cd7288d9076041d8e5bc12f3
Julien Guy <jguy@lbl.gov>
2017-09-15 06:51:29 -0700

    one file per I/O format

--- src/library/specex_psf_io.h
+++ src/library/specex_psf_io.h
@@ -6,28 +6,31 @@
 
 
 namespace specex {
-  
-  void read_psf_xml(specex::PSF_p& psf, const std::string& filename);
-  void write_psf_xml(const specex::PSF_p psf, const std::string& filename);
-  
+
+  // to write an image of the psf
   void write_psf_fits_image(const specex::PSF_p psf, const string& filename, 
 			    const int fiber, const double& wavelength, int oversampling=1);
 
+  // to write the spots coordinates, fluxes ... etc, this is for debugging
+  void write_spots_xml(const std::vector<specex::Spot_p>& spots, const std::string& filename);
+  
+  // write routines
   void write_xtrace_fits_hdu(const specex::PSF& psf, fitsfile *fp, int hdu);
   void write_ytrace_fits_hdu(const specex::PSF& psf, fitsfile *fp, int hdu);
   
+  void write_psf_xml(const specex::PSF_p psf, const std::string& filename);
   void write_psf_fits(const specex::PSF_p psf, fitsfile* fp, int first_hdu=1, std::vector<specex::Spot_p> *spots = NULL);
   void write_psf_fits(const specex::PSF_p psf, const string& filename, std::vector<specex::Spot_p> *spots = NULL);
   void write_psf(const specex::PSF_p psf, const string& filename, std::vector<specex::Spot_p> *spots = NULL);
+
+  // read routines
+  void read_xtrace_fits_hdu(specex::PSF_p psf, fitsfile *fp, int hdu);
+  void read_ytrace_fits_hdu(specex::PSF_p psf, fitsfile *fp, int hdu);
   
-  void read_psf_fits(specex::PSF_p& psf,  fitsfile* fp, int first_hdu=1, int first_bundle=-1, int last_bundle=-1);
+  void read_psf_xml(specex::PSF_p& psf, const std::string& filename);
   void read_psf_fits(specex::PSF_p& psf, const string& filename, int first_bundle=-1, int last_bundle=-1);
   void read_psf(specex::PSF_p& psf, const string& filename, int first_bundle=-1, int last_bundle=-1);
   
-
-  
-
-  void write_spots_xml(const std::vector<specex::Spot_p>& spots, const std::string& filename);
   
 }
 
