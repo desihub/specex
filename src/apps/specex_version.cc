2969655c3f5a1768b5a3ade1d4697f7657ae4fe2
Benjamin Alan Weaver <baweaver@lbl.gov>
2017-11-07 16:10:18 -0700

    binary that prints version string

---
+++ src/apps/specex_version.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <specex_version.h>
+
+using namespace std;
+using namespace specex;
+
+int main ( int argc, char *argv[] ) {
+  cout << version() << endl;
+  return 0
+}
