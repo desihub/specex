27db464d1dbed8569307d23057ff945e000eacf6
Julien Guy <jguy@lbl.gov>
2020-04-15 10:39:52 -0700

    python3

--- python/dump_nist.py
+++ python/dump_nist.py
@@ -4,5 +4,5 @@ import desispec.bootcalib
 
 nist_list=desispec.bootcalib.load_arcline_list(camera="all", vacuum="True", lamps=None)
 for ion,wave,intensity in zip(nist_list["Ion"],nist_list["wave"],nist_list["RelInt"]) :
-    print ion,wave,1,intensity
+    print(ion,wave,1,intensity)
 
