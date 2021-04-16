d45174e4bd52d6ba818be4f0cf2f6765a38e7ce2
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-04-16 15:55:57 -0700

    direct passing of cblas/lapacke options

--- src/specex_blas.c
+++ src/specex_blas.c
@@ -10,61 +10,33 @@
 // returns the dot product of x and y
 // http://www.netlib.org/lapack/explore-html/d5/df6/ddot_8f_source.html
 double specex_dot(int n, const double *x, const double *y){
-  
   return cblas_ddot(n, x, 1, y, 1);
-
 }
 
 // y += alpha*x
 // http://www.netlib.org/lapack/explore-html/d9/dcd/daxpy_8f_source.html
 void specex_axpy(int n, const double *alpha, const double *x, const double *y){
-
-  cblas_daxpy(n, *alpha, x, 1, y, 1);
-  
+  cblas_daxpy(n, *alpha, x, 1, y, 1);  
 }
 
 // A += alpha*x*x**T, where A is a symmetric matrx (only lower half is filled)
 // http://www.netlib.org/lapack/explore-html/d3/d60/dsyr_8f_source.html
 void specex_syr(int n, const double *alpha, const double *x, const double *A){
-
-  CBLAS_LAYOUT layout;
-  CBLAS_UPLO   uplo;
-
-  layout = CblasColMajor;
-  uplo   = CblasLower;
-  
-  cblas_dsyr(layout, uplo, n, *alpha, x, 1, A, n);
-  
+  cblas_dsyr(CblasColMajor, CblasLower, n, *alpha, x, 1, A, n);  
 }
 
 // C = alpha*A**T + beta*C, where A is a symmetric matrx 
 // http://www.netlib.org/lapack/explore-html/dc/d05/dsyrk_8f_source.html
 void specex_syrk(int n, int k, const double *alpha, const double *A, const double *beta,
 		 const double *C){
-
-  CBLAS_LAYOUT layout;
-  CBLAS_UPLO   uplo;
-  CBLAS_TRANSPOSE transa;
-
-  layout = CblasColMajor;
-  uplo   = CblasLower;
-  
-  cblas_dsyrk(layout, uplo, transa, n, k, *alpha, A, n, *beta, C, n);
-  
+  cblas_dsyrk(CblasColMajor, CblasLower, CblasNoTrans, n, k, *alpha, A, n, *beta, C, n);  
 }
 
 // y = alpha*A*x + beta*y
 // http://www.netlib.org/lapack/explore-html/dc/da8/dgemv_8f_source.html
 void specex_gemv(int m, int n, const double *alpha, const double *A, const double *x,
 		 const double *beta, const double *y){
-
-  CBLAS_LAYOUT    layout;
-  CBLAS_TRANSPOSE transa;
-
-  layout = CblasColMajor;
-  transa = CblasNoTrans;
-
-  cblas_dgemv(layout, transa, m, n, *alpha, A, m, x, 1, *beta, y, 1);
+  cblas_dgemv(CblasColMajor, CblasNoTrans, m, n, *alpha, A, m, x, 1, *beta, y, 1);
   
 }
 
@@ -72,15 +44,6 @@ void specex_gemv(int m, int n, const double *alpha, const double *A, const doubl
 // http://www.netlib.org/lapack/explore-html/d7/d2b/dgemm_8f_source.html
 void specex_gemm(int m, int n, int k, const double *alpha, const double *A, const double *B,
 		 const double *beta, const double *C){
-
-  CBLAS_LAYOUT    layout;
-  CBLAS_TRANSPOSE transa, transb;
-
-  layout = CblasColMajor;
-  transa = CblasNoTrans;
-  transb = CblasNoTrans;
-
-  cblas_dgemm(layout, transa, transb, m, n, k, *alpha, A, m, B, k, *beta, C, m);
-  
+  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, *alpha, A, m, B, k, *beta, C, m);  
 }
 
--- src/specex_lapack.c
+++ src/specex_lapack.c
@@ -11,17 +11,13 @@
 //   A * x = b,
 // solution is returned in b, i.e. b --> x, for return value 0
 int specex_posv(int n, const double *A, const double *b){
-  
-  return LAPACKE_dposv(LAPACK_COL_MAJOR, 'L', n, 1, A, n, b, n);
-  
+  return LAPACKE_dposv(LAPACK_COL_MAJOR, 'L', n, 1, A, n, b, n);  
 }
 
 // invert matrix A in place; A := inv(A)
 // http://www.netlib.org/lapack/explore-html/d8/d63/dpotri_8f_source.html
 int specex_potri(int n, const double *A){
-  
-  return LAPACKE_dpotri(LAPACK_COL_MAJOR, 'L', n, A, n);
-  
+  return LAPACKE_dpotri(LAPACK_COL_MAJOR, 'L', n, A, n);  
 }
 
 
