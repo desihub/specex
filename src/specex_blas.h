549aa488112937baa770e97b970c0898ef529380
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-04-16 16:00:15 -0700

    format changes in extern declaration of prototypes

--- src/specex_blas.h
+++ src/specex_blas.h
@@ -1,10 +1,8 @@
 #ifndef SPECEX_BLAS_H_
 #define SPECEX_BLAS_H_
 
-extern "C" {
-  
+extern "C" {  
   double specex_dot(int, const double *, const double *);
-  
   void specex_axpy(int, const double *, const double *, const double *);
   void specex_syr(int, const double *, const double *, const double *);
   void specex_syrk(int, int, const double *, const double *, const double *,
@@ -12,8 +10,7 @@ extern "C" {
   void specex_gemv(int, int, const double *, const double *, const double *,
 		   const double *, const double*);
   void specex_gemm(int, int, int, const double *, const double *, const double *,
-		   const double *, const double*);
-  
+		   const double *, const double*);  
 }
 
 #endif
--- src/specex_lapack.h
+++ src/specex_lapack.h
@@ -2,10 +2,8 @@
 #define SPECEX_LAPACK_H_
 
 extern "C" {
-  
   int specex_posv(int, const double *, const double *);
-  int specex_potri(int, const double *);
-  
+  int specex_potri(int, const double *);  
 }
 
 #endif
